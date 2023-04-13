5f48b5c77a6229feeb9395443a7c7cf84b09649b
sqxy090123 <120554320+sqxy090123@users.noreply.github.com>
2023-04-13 23:16:16 +0800

    Update get_ip.py
    Signed-off-by: sqxy090123 <120554320+sqxy090123@users.noreply.github.com>

--- py/about/get_ip.py
+++ py/about/get_ip.py
@@ -1,4 +1,19 @@
 #d99a887f-da0b-11ed-9794-416243f2837a
 import socket
-def __init__(self):
-
+def about():
+  all = []
+  name = socket gethostname()
+  ip = socket.gethostbyname(name)
+  if name == "localhost":
+    all.append("设备:手机/Turmux")
+  else:
+    all.append("设备:电脑")
+  all.append("IP:{}".format(ip))
+  try:
+    from dll.system import find_file
+    af = find_file.list_all()
+    all.append("目录:{}".format("".join(str(af))))
+  except Exception as Err:
+    all.append("目录:{}".format(str(Err)))
+  
+  return all
