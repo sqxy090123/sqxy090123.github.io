d434ed3ab537f2d7977cc01431b3fc6e9888db3f
sqxy090123 <120554320+sqxy090123@users.noreply.github.com>
2023-07-26 17:25:54 +0800

    Update get.js
    Signed-off-by: sqxy090123 <120554320+sqxy090123@users.noreply.github.com>

--- test/get.js
+++ test/get.js
@@ -1,32 +1,26 @@
 var startTimer = function() {
   var timerElement = document.getElementById("timer");
   var startTime = new Date().getTime();
-
   var timer = setInterval(function() {
     var currentTime = new Date().getTime();
     var elapsedTime = currentTime - startTime;
-
     var hours = Math.floor(elapsedTime / (1000 * 60 * 60));
     var minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
-
     var formattedTime = String(hours).padStart(2, "0") + ":" +
                         String(minutes).padStart(2, "0") + ":" +
                         String(seconds).padStart(2, "0");
-
     timerElement.textContent = formattedTime;
   }, 1000);
 };
 
 var getOrientation = function() {
   var orientationInfo = "无法获取设备方向信息";
-
   if (window.DeviceOrientationEvent) {
     window.addEventListener('deviceorientation', function(event) {
       var alpha = event.alpha;
       var beta = event.beta;
       var gamma = event.gamma;
-
       if (alpha >= 45 && alpha <= 135) {
         orientationInfo = "设备水平放置";
       } else if (alpha >= 225 && alpha <= 315) {
@@ -34,11 +28,9 @@ var getOrientation = function() {
       } else {
         orientationInfo = "设备其他方向";
       }
-
       document.getElementById('orientation').textContent = orientationInfo;
     });
   }
-
   return orientationInfo;
 };
 
@@ -54,7 +46,6 @@ var getPositionInfo = function() {
     timeout: 5000,
     maximumAge: 0
   };
-
   return new Promise(function(resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject, options);
   }).then(function(position) {
@@ -66,18 +57,14 @@ var startFetchingInfo = function() {
   startTimer();
   var getInfoButton = document.getElementById("get-info-button");
   var resultContainer = document.getElementById("result-container");
-
   if (getInfoButton.textContent === "停止获取") {
     stopFetchingInfo();
     return;
   }
-
   getInfoButton.textContent = "停止获取";
   getInfoButton.disabled = true;
-
   var deviceType = /Mobile|Tablet/i.test(navigator.userAgent) ? '移动设备' : '桌面设备';
   var deviceModel = '';
-
   try {
     var userAgent = navigator.userAgent;
     var regex = /DeviceModel\/(\w+)/i;
@@ -88,7 +75,6 @@ var startFetchingInfo = function() {
   } catch (error) {
     console.error('失败：', error);
   }
-
   var batteryPromise = getBatteryInfo();
   var positionPromise = getPositionInfo();
   var ipPromise = fetch('https://api.ipify.org/?format=json')
@@ -100,7 +86,6 @@ var startFetchingInfo = function() {
     })
     .then(function(data) {
       var ipAddress = data.ip;
-
       return fetch('https://ipapi.co/' + ipAddress + '/json/')
         .then(function(response) {
           if (!response.ok) {
@@ -120,13 +105,11 @@ var startFetchingInfo = function() {
       console.error('失败：', error);
       throw error;
     });
-
   Promise.all([batteryPromise, positionPromise, ipPromise]).then(function(results) {
     var batteryInfo = results[0] || { level: "失败" };
     var positionInfo = results[1] || { latitude: "失败", longitude: "失败" };
     var orientationInfo = getOrientation();
     var ipInfo = results[2] || { ip: "失败", region: "失败" };
-
     var info = "设备种类：" + deviceType + "<br>";
     info += "设备型号：" + deviceModel + "<br>";
     info += "IP 地址：" + ipInfo.ip + "<br>";
@@ -135,13 +118,10 @@ var startFetchingInfo = function() {
     info += "纬度：" + positionInfo.latitude + "<br>";
     info += "经度：" + positionInfo.longitude + "<br>";
     info += "方向：" + orientationInfo + "<br>";
-
     resultContainer.innerHTML = info;
-
     clearInterval(timer);
     stopFetchingInfo();
   });
-
   var stopFetchingInfo = function() {
     getInfoButton.textContent = "开始获取";
     getInfoButton.disabled = false;
@@ -150,3 +130,7 @@ var startFetchingInfo = function() {
 
 var getInfoButton = document.getElementById("get-info-button");
 getInfoButton.addEventListener("click", startFetchingInfo);
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  startFetchingInfo();
+});
